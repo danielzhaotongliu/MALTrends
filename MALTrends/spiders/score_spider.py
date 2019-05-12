8e60a3529b902fe85dc76db06e355715f5dd8cb7
danielzhaotongliu <ztliu@edu.uwaterloo.ca>
2019-05-12 19:11:12 -0400

    basic MAL spider to save file

--- MALTrends/spiders/score_spider.py
+++ MALTrends/spiders/score_spider.py
@@ -3,9 +3,20 @@ import requests
 
 class ScoreSpider(scrapy.Spider):
     name = 'score_spider'
-    
+
     def start_requests(self):
-        pass
-        
+        r = requests.get('https://api.jikan.moe/v3/anime/%s' % self.id)
+        data = r.json()
+        anime_urls = [
+            data['url'],
+            'https://myanimelist.net/anime/%s' % self.id
+        ]
+        for url in anime_urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
     def parse(self, response):
-        pass
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
