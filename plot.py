d376a5c151bb146678be87920743f4485e6de953
danielzhaotongliu <ztliu@edu.uwaterloo.ca>
2019-05-17 15:09:15 -0400

    added data visualization using plotly

--- plot.py
+++ plot.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 import json
+import plotly.graph_objs as go
+import plotly.offline as ply
 
 # load in the data
 times = []
@@ -13,3 +15,29 @@ with open('snapshots.jl', 'r') as f:
         scores.append(snapshot['score'])
         scores_count.append(snapshot['score_count'])
 
+# create traces - data collections
+trace = go.Scatter(
+    x = times, 
+    y = scores,
+    name = 'scores'
+)
+
+# pack the data
+data = [trace]
+
+# configure layout
+layout = go.Layout(
+    title = 'MyAnimeList Scores Trend',
+    xaxis = dict(
+        title = 'Date',
+        showticklabels=True
+    ),
+    yaxis = dict(
+        title = 'Score',
+        showticklabels=True
+    )
+)
+
+# plot as html
+fig = go.Figure(data=data, layout=layout)
+ply.plot(fig, filename='scores_trend.html')
