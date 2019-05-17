a3d223e1e4c47d063b34d9971f2591e56c0db20d
danielzhaotongliu <ztliu@edu.uwaterloo.ca>
2019-05-17 16:55:05 -0400

    fixed plotly time series graph

--- plot.py
+++ plot.py
@@ -1,25 +1,28 @@
 from datetime import datetime
 import json
 import plotly.graph_objs as go
-import plotly.offline as ply
+import plotly.offline as py
 
 # load in the data
+data_trends = {}
 times = []
 scores = []
-scores_count = []
 with open('snapshots.jl', 'r') as f:
     for line in f:
         snapshot = json.loads(line)
-        time = datetime.utcfromtimestamp(snapshot['timestamp'])
-        times.append(time)
-        scores.append(snapshot['score'])
-        scores_count.append(snapshot['score_count'])
+        time = datetime.utcfromtimestamp(snapshot['timestamp']).strftime('%Y-%m-%d %H:%M:%S')
+        data_trends[time] = snapshot['score']
+
+for time, score in sorted(data_trends.items()):
+    times.append(time)
+    scores.append(score)
 
 # create traces - data collections
 trace = go.Scatter(
     x = times, 
     y = scores,
-    name = 'scores'
+    name = 'scores',
+    mode = 'lines+markers'
 )
 
 # pack the data
@@ -30,14 +33,12 @@ layout = go.Layout(
     title = 'MyAnimeList Scores Trend',
     xaxis = dict(
         title = 'Date',
-        showticklabels=True
     ),
     yaxis = dict(
         title = 'Score',
-        showticklabels=True
     )
 )
 
 # plot as html
 fig = go.Figure(data=data, layout=layout)
-ply.plot(fig, filename='scores_trend.html')
+py.plot(fig, filename='scores_trend.html')
